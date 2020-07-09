e5a2abd11d734d62a6b4d6469da56e16219bbd7e
Candi <13277979+cdfranck@users.noreply.github.com>
2020-07-09 09:02:47 -0500

    Update Power-On.ps1
    added comments

--- Power-On.ps1
+++ Power-On.ps1
@@ -1,3 +1,8 @@
+#Script to power on servers by delivery group, created for scheduled reboot failure to power on servers
+#Servers must be power managed from the site
+#Script should be run from the DDC with elevated rights
+# make sure to edit line 13 for your DDC name (-AdminAddress)
+
 Add-PSSnapin Citrix*
 
 Get-BrokerDesktopGroup | Select -ExpandProperty Name | out-file c:\temp\deliverygroups.txt
