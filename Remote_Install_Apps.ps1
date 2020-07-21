fbc8481c9b0dd9dcfd1c81f2ed2afe6c431698ae
Richard Chisholm <64597465+hs01838@users.noreply.github.com>
2020-07-21 09:18:04 -0500

    Add files via upload

---
+++ Remote_Install_Apps.ps1
@@ -0,0 +1,10 @@
+﻿
+#Example of Scriptblock - "msiexec.exe /i FILENAME.MSI /QN
+# "FILE.CMD"
+
+$servers = get-content "PATH_TO_SERVER_LIST"
+
+Foreach ($server in $servers){
+Write-Host -BackgroundColor white -ForegroundColor blue "Updating $server"
+Invoke-Command -ComputerName $server -ScriptBlock {(& cmd.exe /c "PATH_TO_INSTALL_FILE")}
+}
