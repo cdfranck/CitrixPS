080c347a9445ed3ef8df0f9ada0cdaef341a90f9
Richard Chisholm <64597465+hs01838@users.noreply.github.com>
2022-06-22 11:17:28 -0500

    Update Remote_Install_Apps.ps1

--- Remote_Install_Apps.ps1
+++ Remote_Install_Apps.ps1
@@ -8,3 +8,15 @@ Foreach ($server in $servers){
 Write-Host -BackgroundColor white -ForegroundColor blue "Updating $server"
 Invoke-Command -ComputerName $server -ScriptBlock {(& cmd.exe /c "PATH_TO_INSTALL_FILE")}
 }
+
+OR 
+
+#Example of Scriptblock - "msiexec.exe /i FILENAME.MSI /QN
+# "FILE.CMD"
+# "Added -AsJob command to install to list of servers simultaneously"
+
+$servers = get-content "PATH_TO_SERVER_LIST"
+
+Foreach ($server in $servers){
+Write-Host -BackgroundColor white -ForegroundColor blue "Updating $server"
+Invoke-Command -ComputerName $server -ScriptBlock -AsJob {(& cmd.exe /c "PATH_TO_INSTALL_FILE")}
