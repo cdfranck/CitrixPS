e8d500035baf0a3da8251d12fa32680d4d7a44ef
Richard Chisholm <64597465+hs01838@users.noreply.github.com>
2022-06-22 11:15:14 -0500

    Add files via upload
    This allows remote installation of an application

---
+++ Remote_Install.ps1
@@ -0,0 +1,11 @@
+﻿$servers = get-content “ServersList.txt”
+
+
+
+Foreach ($server in $servers){
+
+Write-Host -BackgroundColor white -ForegroundColor blue "Updating $server"
+
+Invoke-Command -ComputerName $server -ScriptBlock -AsJob {(& cmd.exe /c " Path to Powershell scrip/CMD File/MSI/or ARP Entry")}
+
+}
