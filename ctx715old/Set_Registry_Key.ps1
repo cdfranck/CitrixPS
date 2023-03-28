d172f444c73ce14045283d625457216b9721e498
Candi <13277979+cdfranck@users.noreply.github.com>
2023-03-28 11:27:19 -0500

    archive older files
    archive older files

---
+++ ctx715old/Set_Registry_Key.ps1
@@ -0,0 +1,10 @@
+﻿# On all remote machines that need their info changed
+Set-ExecutionPolicy Unrestricted
+Enable-PSRemoting # Say yes to all prompts
+
+$Computers = Get-Content "Path to Servers.txt"
+
+
+foreach ($computer in $computers) {
+Invoke-Command -AsJob $Computers -ScriptBlock {$path = "HKLM:\REGISTRY KEY";Set-ItemProperty -Path $path -Name “Name of Key” -Value “x”}
+}
--- get-dotnetupdates.ps1
+++
@@ -1,2 +0,0 @@
-﻿Get-ChildItem 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Updates\Microsoft .NET Framework 4 Client Profile' | Select Name
-Get-ChildItem 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Updates\Microsoft .NET Framework 4 Extended' | Select Name
